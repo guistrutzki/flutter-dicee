9d13df45469d355d536eda139077ead2ea3564a1
Guilherme Strutzki <guilherme.strutzki@shareeat.com.br>
2020-02-16 21:21:11 -0300

    :sparkles: Add dices layout

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset('images/dice1.png'),
+            )
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
